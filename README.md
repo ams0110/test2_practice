e7c962f7c94666cb4cfba7f095e5accefa6282a7
Amanda Skalitzky <ams0110@auburn.edu>
2018-07-27 07:24:33 -0500

    committed a catkin workspace to see if I can upload it to github.

---
+++ README.md
@@ -0,0 +1,1 @@
+Creating a new file to see if I can get git to work. 
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/amanda/test2_practice/src
+# Build directory: /home/amanda/test2_practice/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(beginner_tutorials)
